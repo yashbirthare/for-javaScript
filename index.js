2184a112987353a054f4d51944354f3f191448cb
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-12-27 23:03:04 +0530

    done

---
+++ index.js
@@ -0,0 +1,36 @@
+// var x = "hello my nama is yash birthare <br>";
+    
+
+// var x , y , z; 
+
+
+ var x = " yash <br>"
+ 
+// y = "birthare <br>"
+// z = "miss u maa"
+
+
+
+document.write(x)
+document.write(x)
+document.write(y)
+document.write(y)
+document.write(z)
+
+
+
+
+
+
+
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
+// document.write(x);
