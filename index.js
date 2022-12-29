53b42701ade3784891f39c40cfe30668b258610a
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-12-29 15:34:33 +0530

    done

--- index.js
+++ index.js
@@ -1,28 +1,98 @@
+var u = "hello"
+var v = true
+var w = 500
+var x = ["HTML" , "CSS" , "JS", "REACT.JS",]
+var y = {
+    name: "yash birthare",
+    city: "guna"
+}
 
-// var x = "hello frd i am yash from guna <br>"
+document.write(u)
+document.write("<br>")
+document.write(typeof u)
+document.write("<br> <br>")
 
-// var y = "hello <br>"
+document.write(v)
+document.write("<br>")
+document.write(typeof v)
+document.write("<br> <br>")
+
+document.write(w)
+document.write("<br>")
+document.write(typeof w)
+document.write("<br> <br>")
+
+document.write(x)
+document.write("<br>")
+document.write(typeof x)
+document.write("<br> <br>")
+
+document.write(y)
+document.write("<br>")
+document.write(typeof y)
+document.write("<br> <br>")
+
+// data type ========================================
+
+// ++++++++++++++++++++++++++++++++++++==============================================++++++++++++++++++++++++++++++++++++++++operators+++++++++++++++++ arithmetic operators
+var a = 10;
+var b =  3;
+
+var c = (a + b) * 2;
+
+document.write(c);
+
+document.write("<br> <br>");
+
+// wrong
+var a = 10;
+var b =  3;
+
+var c = (a + b) * 2;
+
+document.write(c);
+
+
+// ========================================================================================= assignment operators
+
+document.write("<br> <br>");
+
+var a = 10;
+var b = 3;
+
+a -=  b;
+
+document.write(a)
+
+
+// ===================================================================================== 
+
+var a = 10;
+var b = 3;
+
+a %= b
+
+// console.time("texting")
+// console.warn("you are wrong");
+// console.error("you are wrong");
+// console.warn("you are wrong");
+// console.error("you are wrong");
+// console.warn("you are wrong");
+// console.error("you are wrong");
+// console.warn("you are wrong");
+// console.error("you are wrong");
+// console.warn("you are wrong");
+// console.error("you are wrong");
+// console.warn("you are wrong");
+// console.timeEnd("texting")
+// console.error("you are wrong");
+
+// console.clear();
 
 
 
 
 
-// document.write(x)
-// document.write(x)
-// document.write(x)
-// document.write(x)
-// document.write(y)
-// document.write(y)
-// document.write(y)
-// document.write(y)
-// document.write(y)
 
 
-const  yash = "i am yash and i am from guna";
-
-
-
-
-
-
-document.write(yash)
+// ===============================================================================================
