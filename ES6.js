e326f0a459102122fef9eb5f439ddea47112922d
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2023-01-11 19:03:34 +0530

    done

---
+++ ES6.js
@@ -0,0 +1,63 @@
+// function hello () {
+//     document.write("my nam is yash")
+// }
+
+ 
+// hello()
+ 
+// document.write("<br><br>")
+
+
+// const hey = function () {
+//     document.write("my name is bhanu")
+// }
+
+
+// hey()
+
+// document.write("<br><br>")
+
+// const Arrow = () => 
+// document.write("my name is king")
+
+// Arrow()
+
+// document.write("<br><br>")
+// // ==================================Arrow function===================================================
+
+// let welcome = (name, age) => {
+//     return `hello $ welcome ${name} - ${age}`;
+    
+// }
+// document.write(welcome("yash ", 25));
+
+// // =====================================================================================
+// document.write("<br><br>")
+
+// let user = ["tinku",25,"guna","male", 250000];
+//   let [name="ashu" , age, city ="indore",gender,selery] = user;
+
+//   document.write(name)
+//   document.write("<br><br>")
+//   document.write(age)
+//   document.write("<br><br>")
+//   document.write(city)
+//   document.write("<br><br>")
+//   document.write(gender)
+//   document.write("<br><br>")
+//   document.write(selery)
+
+  
+// ================================================================================
+let user = ["tinku",25,"guna","male", 250000];
+  let [name="ashu" , age, city ="indore",gender,selery] = user;
+
+  document.write(name)
+  document.write("<br><br>")
+  document.write(age)
+  document.write("<br><br>")
+  document.write(city)
+  document.write("<br><br>")
+  document.write(gender)
+  document.write("<br><br>")
+  document.write(selery)
