9416706892c72cbc61bc50b56cd1100bc0f23290
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2023-01-12 00:36:20 +0530

    done

--- ES6.js
+++ ES6.js
@@ -48,16 +48,117 @@
 //   document.write(selery)
 
   
-// ================================================================================
-let user = ["tinku",25,"guna","male", 250000];
-  let [name="ashu" , age, city ="indore",gender,selery] = user;
+// // ================================================================================
 
-  document.write(name)
-  document.write("<br><br>")
-  document.write(age)
-  document.write("<br><br>")
-  document.write(city)
-  document.write("<br><br>")
-  document.write(gender)
-  document.write("<br><br>")
-  document.write(selery)
+// let user = ["tinku", 25, "guna","male",25000] 
+
+// let [name = "bhanu", age, city,gender,selery] = user;
+
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+// console.log(gender)
+// console.log(selery)
+
+
+// // ==============================================================
+// normale function
+
+// function user ([name, age, city,gender,selery]) {
+
+//   console.log(name)
+//   console.log(age)
+//   console.log(city)
+//   console.log(gender)
+//   console.log(selery)
+
+// }
+
+// user(["tinku", 25, "guna","male",25000] );
+
+// const user = ([name, age, city,gender,selery]) => {
+
+//   console.log(name)
+//   console.log(age)
+//   console.log(city)
+//   console.log(gender)
+//   console.log(selery)
+
+// }
+
+// user(["tinku", 25, "guna","male",25000] );
+
+
+// const user = ([name, age, city,gender,selery]) => {
+
+//   console.log(name)
+//   console.log(age)
+//   console.log(city)
+//   console.log(gender)
+//   console.log(selery)
+
+// }
+
+// user(["tinku", 25, "guna","male",25000] );
+
+
+
+
+// const user = () => {
+
+//   return ["tinku", 25, "guna","male",25000];
+
+
+// }
+
+// let [name, age, city,gender,selery] = user();
+
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.log(gender);
+// console.log(selery);
+
+
+// =============================================================================
+
+// object
+
+
+
+// const user = {name: "yash", age: 25, city:"guna",}
+
+// const {name, age, city} = user;
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+
+
+
+// const user = ({name, age, city}) => {
+//           console.log(name)
+//           console.log(age)
+//           console.log(city)
+// }
+
+// user({name: "yash", age: 25, city:"guna",})
+
+
+
+
+// const user = () => {
+//   return {name: "yash", age: 25, city:"guna"}
+// }
+
+// const {name, age, city} = user();
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+
+
+
+
