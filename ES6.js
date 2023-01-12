b6cb3040c9b4817c295f20b256d4634688b417c8
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2023-01-12 23:17:18 +0530

    done

--- ES6.js
+++ ES6.js
@@ -1,203 +1,185 @@
-// function hello () {
-//     document.write("my nam is yash")
-// }
+function hello () {
+    document.write("my nam is yash")
+}
 
  
-// hello()
+hello()
  
-// document.write("<br><br>")
+document.write("<br><br>")
 
 
-// const hey = function () {
-//     document.write("my name is bhanu")
-// }
+const hey = function () {
+    document.write("my name is bhanu")
+}
 
 
-// hey()
+hey()
 
-// document.write("<br><br>")
+document.write("<br><br>")
 
-// const Arrow = () => 
-// document.write("my name is king")
+const Arrow = () => 
+document.write("my name is king")
 
-// Arrow()
+Arrow()
 
-// document.write("<br><br>")
-// // ==================================Arrow function===================================================
+document.write("<br><br>")
+// ==================================Arrow function===================================================
 
-// let welcome = (name, age) => {
-//     return `hello $ welcome ${name} - ${age}`;
+let welcome = (name, age) => {
+    return `hello $ welcome ${name} - ${age}`;
     
-// }
-// document.write(welcome("yash ", 25));
+}
+document.write(welcome("yash ", 25));
 
-// // =====================================================================================
-// document.write("<br><br>")
+// =====================================================================================
+document.write("<br><br>")
 
-// let user = ["tinku",25,"guna","male", 250000];
-//   let [name="ashu" , age, city ="indore",gender,selery] = user;
+let user = ["tinku",25,"guna","male", 250000];
+  let [name="ashu" , age, city ="indore",gender,selery] = user;
 
-//   document.write(name)
-//   document.write("<br><br>")
-//   document.write(age)
-//   document.write("<br><br>")
-//   document.write(city)
-//   document.write("<br><br>")
-//   document.write(gender)
-//   document.write("<br><br>")
-//   document.write(selery)
+  document.write(name)
+  document.write("<br><br>")
+  document.write(age)
+  document.write("<br><br>")
+  document.write(city)
+  document.write("<br><br>")
+  document.write(gender)
+  document.write("<br><br>")
+  document.write(selery)
 
   
-// // ================================================================================
+// ================================================================================
 
-// let user = ["tinku", 25, "guna","male",25000] 
+let user = ["tinku", 25, "guna","male",25000] 
 
-// let [name = "bhanu", age, city,gender,selery] = user;
-
-
-// console.log(name)
-// console.log(age)
-// console.log(city)
-// console.log(gender)
-// console.log(selery)
+let [name = "bhanu", age, city,gender,selery] = user;
 
 
-// // ==============================================================
-// normale function
-
-// function user ([name, age, city,gender,selery]) {
-
-//   console.log(name)
-//   console.log(age)
-//   console.log(city)
-//   console.log(gender)
-//   console.log(selery)
-
-// }
-
-// user(["tinku", 25, "guna","male",25000] );
-
-// const user = ([name, age, city,gender,selery]) => {
-
-//   console.log(name)
-//   console.log(age)
-//   console.log(city)
-//   console.log(gender)
-//   console.log(selery)
-
-// }
-
-// user(["tinku", 25, "guna","male",25000] );
+console.log(name)
+console.log(age)
+console.log(city)
+console.log(gender)
+console.log(selery)
 
 
-// const user = ([name, age, city,gender,selery]) => {
+// ==============================================================
+normale function
 
-//   console.log(name)
-//   console.log(age)
-//   console.log(city)
-//   console.log(gender)
-//   console.log(selery)
+function user ([name, age, city,gender,selery]) {
 
-// }
+  console.log(name)
+  console.log(age)
+  console.log(city)
+  console.log(gender)
+  console.log(selery)
 
-// user(["tinku", 25, "guna","male",25000] );
+}
 
+user(["tinku", 25, "guna","male",25000] );
 
+const user = ([name, age, city,gender,selery]) => {
 
+  console.log(name)
+  console.log(age)
+  console.log(city)
+  console.log(gender)
+  console.log(selery)
 
-// const user = () => {
+}
 
-//   return ["tinku", 25, "guna","male",25000];
+const user = () => {
 
-
-// }
-
-// let [name, age, city,gender,selery] = user();
-
-
-// console.log(name);
-// console.log(age);
-// console.log(city);
-// console.log(gender);
-// console.log(selery);
+  return ["tinku", 25, "guna","male",25000];
 
 
-// =============================================================================
+}
 
-// object
+let [name, age, city,gender,selery] = user();
+
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(gender);
+console.log(selery);
+
+
+=============================================================================
+
+object
 
 
 
-// const user = {name: "yash", age: 25, city:"guna",}
+const user = {name: "yash", age: 25, city:"guna",}
 
-// const {name, age, city} = user;
+const {name, age, city} = user;
 
-// console.log(name)
-// console.log(age)
-// console.log(city)
+console.log(name)
+console.log(age)
+console.log(city)
 
 
 
-// const user = ({name, age, city}) => {
-//           console.log(name)
-//           console.log(age)
-//           console.log(city)
-// }
+const user = ({name, age, city}) => {
+          console.log(name)
+          console.log(age)
+          console.log(city)
+}
 
-// user({name: "yash", age: 25, city:"guna",})
+user({name: "yash", age: 25, city:"guna",})
 
 
 
 
-// const user = () => {
-//   return {name: "yash", age: 25, city:"guna"}
-// }
+const user = () => {
+  return {name: "yash", age: 25, city:"guna"}
+}
 
-// const {name, age, city} = user();
+const {name, age, city} = user();
 
-// console.log(name)
-// console.log(age)
-// console.log(city)
+console.log(name)
+console.log(age)
+console.log(city)
 
 
 
 
 
 
-// if ( 1==1) {
+if ( 1==1) {
 
-//     var x = "yash"
+    var x = "yash"
     
-// }
+}
 
-// console.log(x)
+console.log(x)
 
-// =========Global scope ====================
+=========Global scope ====================
 
 
-// if ( 1==1) {
+if ( 1==1) {
 
-//     let x = "yash"
-//     console.log(x)
+    let x = "yash"
+    console.log(x)
     
-// }
+}
 
 
-// ==========================block scope ==================
+==========================block scope ==================
 
-// const x = "yash birthare"
-// const y = 25
+const x = "yash birthare"
+const y = 25
 
-// document.write("hello and welconme" + x , "what is your age " + y )
-// console.log("hello and welconme" + x , "what is your age " + y )
+document.write("hello and welconme" + x , "what is your age " + y )
+console.log("hello and welconme" + x , "what is your age " + y )
 
-// =============old======================
+=============old======================
 
-// const x = "yash birthare"
-// const y = 25
+const x = "yash birthare"
+const y = 25
 
-// document.write(`hello and welcome ${x} what is your age ${y}`)
-// console.log(`hello and welcome ${x} what is your age ${y}`)
+document.write(`hello and welcome ${x} what is your age ${y}`)
+console.log(`hello and welcome ${x} what is your age ${y}`)
 
-// =========Template String ===================
+=========Template String ===================
 
